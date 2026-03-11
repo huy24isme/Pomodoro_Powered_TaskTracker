f423b9d1410dee7430e0f09378308654f4f27152
Ha Hai Cuong <hhc9104@gmail.com>
2026-03-12 01:17:09 +0700

    feat: remove redundant index.js

--- backend/src/config/index.js
+++
@@ -1,4 +0,0 @@
-export { env } from './env.config';
-export { corsOptions } from './cors.config';
-export { connectMongoDB, mongoose } from './mongodb.config';
-export { connectRedis, getRedisClient, disconnectRedis } from './redis.config';
